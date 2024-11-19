0523602569bd174507bfe7338148aff9bb55c18d
hanumanqwert <saieee2929@gmail.com>
2024-11-19 19:25:08 +0530

    initial commit

---
+++ src/test/java/com/wipro/services/WiproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
